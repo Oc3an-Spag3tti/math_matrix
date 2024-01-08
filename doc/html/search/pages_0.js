897dccb122ff7e864536e13f635bff8623f30ee7
Oc3an-Spag3tti <daniilgurin20@gmial.com>
2024-01-08 09:43:37 +0100

    second commit

---
+++ doc/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['math_5fmatrix_0',['math_matrix',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ lib/math_matrix.c
@@ -0,0 +1,7 @@
+typedef struct nml_mat_s {
+  unsigned int num_rows;
+  unsigned int num_cols;
+  double **data;
+  int is_square;
+} nml_mat;
+
